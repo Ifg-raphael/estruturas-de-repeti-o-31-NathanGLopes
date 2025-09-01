ec49c9ce79a952ffef923da3a7f8f13b41ef987e
NathanGLopes <nathangclopes@gmail.com>
2025-09-01 15:12:18 -0300

    Atividade 5 realizada

--- calcula_somatorio.py
+++ calcula_somatorio.py
@@ -1,1 +1,9 @@
-# Sua solução aqui
+# lê o valor de n
+n = int(input("Digite o valor de n: "))
+soma = 0
+# laço de 2 até n (inclusive)
+for i in range(2, n + 1):
+    soma = soma + (i / (i - 1))
+print("O valor do somatório é:", soma)
+# calcula o somatório de 2/(2-1) + 3/(3-1) + ... + n/(n-1)
+# e imprime o resultado
